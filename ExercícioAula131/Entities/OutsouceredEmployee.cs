120230abba72b78aacf439a05acd829c5e0399e4
JoelDickel <64815075+JoelDickel@users.noreply.github.com>
2023-01-09 12:32:29 -0300

    First Commit
    Subindo tudo

---
+++ ExercícioAula131/Entities/OutsouceredEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercícioAula131.Entities
+{
+    class OutsouceredEmployee : Employee
+    {
+        public double AditionarCharg { get; set; }
+
+        public OutsouceredEmployee()
+        {
+
+        }
+
+        public OutsouceredEmployee(string name, int hours, double valuePerHour, double aditionarCharg) : base (name, hours, valuePerHour)
+        {
+            AditionarCharg = aditionarCharg;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AditionarCharg;
+        }
+
+
+    }
+}
